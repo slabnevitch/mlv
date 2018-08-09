3402aa10462b83c6d6f919be43115b35b10c4805
slabnevitch <handofschpilrain@mail.ru>
2018-08-09 20:34:51 +0500

    werer

--- js/common.js
+++ js/common.js
@@ -34,6 +34,22 @@ $(function() {
 			
 		// end assortment-visual click disable
 
+		is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
+	    is_explorer = navigator.userAgent.indexOf('MSIE') > -1;
+	    is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
+	    is_safari = navigator.userAgent.indexOf("Safari") > -1;
+	    is_opera = navigator.userAgent.indexOf("Presto") > -1;
+	    is_mac = (navigator.userAgent.indexOf('Mac OS') != -1);
+	    is_windows = !is_mac;
+
+	    if (is_chrome && is_safari){
+	      is_safari=false;
+	    }
+
+	    if (is_safari || is_windows){
+	      $('h1, h2').addClass('l-spacing');
+	    }
+
 		// assortment hover detect
 			var isMobile = {
 				Android:        function() { return navigator.userAgent.match(/Android/i) ? true : false; },
